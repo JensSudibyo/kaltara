d5ec4eff1a68aa809c0cb2981ed932f305aa74e2
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2024-12-08 03:02:01 +0800

    Update histats.js

--- histats.js
+++ histats.js
@@ -1,12 +1,11 @@
-<!-- Histats.com  START  (aync)-->
-<script type="text/javascript">var _Hasync= _Hasync|| [];
+// Histats.com START (aync)
+var _Hasync= _Hasync|| [];
 _Hasync.push(['Histats.start', '1,4915046,4,0,0,0,00010000']);
 _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
 (function() {
-var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//s10.histats.com/js15_as.js');
-(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4915046&101" alt="" border="0"></a></noscript>
-<!-- Histats.com  END  -->
+    var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+    hs.src = ('//s10.histats.com/js15_as.js');
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();
+// Histats.com END
