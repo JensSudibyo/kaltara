32da719552d91ca2e02b69404ea1bb56e9d663b3
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2025-04-14 20:17:05 +0800

    Delete vip/allowedhost.js

--- vip/allowedhost.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    const allowedHost = ['streamkaltaraid.online'];
-    const currentHost = window.location.hostname;
-
-    if (!allowedHost.includes(currentHost)) {
-        console.error('Access denied: Unauthorized host');
-        window.location.href = 'https://kltraid.pages.dev/';
-    }
-})();
