0baf4f389c9ecd4ae3c5b779e265cf84937a1a14
JensSudibyo <135489722+JensSudibyo@users.noreply.github.com>
2025-04-14 19:21:12 +0800

    Update allowedhost.js

--- vip/allowedhost.js
+++ vip/allowedhost.js
@@ -1,10 +1,9 @@
-// /js/host-check.js
-export function enforceAllowedHost() {
-    const allowedHost = ['bikinbaru96.blogspot.com'];
+(function () {
+    const allowedHost = ['streamkaltaraid.online'];
     const currentHost = window.location.hostname;
 
     if (!allowedHost.includes(currentHost)) {
         console.error('Access denied: Unauthorized host');
-        window.location.href = 'https://akusukagratisanlo.blogspot.com';
+        window.location.href = 'https://kltraid.pages.dev/';
     }
-}
+})();
